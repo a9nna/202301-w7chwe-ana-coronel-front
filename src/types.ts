5aeae4bbc8eb0fc6d892afce1b39be7e34dbd5c8
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-25 19:37:45 +0100

    Create UsersDataStructure interface

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface UsersDataStructure {
+  id: string;
+  image: string;
+  username: string;
+  password: string;
+  email: string;
+  enemies: {};
+  friends: {};
+  isLogged: boolean;
+}
